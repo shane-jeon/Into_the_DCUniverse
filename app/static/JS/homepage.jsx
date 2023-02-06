06ae1fa94b5adf66a2f9f2e95edf98e7a1d0ff02
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-02-06 09:13:14 -0800

    created first React component

---
+++ app/static/JS/homepage.jsx
@@ -0,0 +1,10 @@
+'use strict';
+
+function Homepage() {
+  return (
+    <h1>Homepage goes here</h1>
+  )
+  
+}
+
+ReactDOM.createRoot(document.getElementById("root-homepage")).render(<Homepage />);
